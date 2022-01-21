43243b0f5ba118d78c3b949d3ec62ca7c5dfb52d
sagarbhatt.ons1087@gmail.com <sagarbhatt.ons1087@gmail.com>
2022-01-21 14:17:14 -0700

    Pre-project Test

---
+++ src/main/java/Project_1_Cinema_Management_Application/Array_Operation.java
@@ -0,0 +1,38 @@
+/**
+ * Problem statement:
+ *
+ * Write a method named addValueByIndex.
+ *
+ * The method should take an array of longs and add value to the specified element by its index.
+ *
+ * Here is a description of the parameters:
+ *
+ * an array of longs;
+ * the index of an element (int);
+ * a value for adding (long).
+ * The method must return nothing.
+ *
+ * The following invocation should work correctly:
+ *
+ * addValueByIndex(array, index, value);
+ * Where array is an array of longs, index is an integer variable, value is a long value for adding.
+ *
+ * Sample Input 1:
+ *
+ * 1 1 1 1 1
+ * 2 100
+ * Sample Output 1:
+ *
+ * 1 1 101 1 1
+ */
+
+
+package Project_1_Cinema_Management_Application;
+
+public class Array_Operation {
+
+    public static void main(String[] args) {
+
+    }
+
+}
