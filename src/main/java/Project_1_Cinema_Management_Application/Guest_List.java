4b017043968ea47994afb80262be8c1801db467e
sagarbhatt.ons1087@gmail.com <sagarbhatt.ons1087@gmail.com>
2022-01-28 13:52:40 -0700

    Problem to solve - Guest List

---
+++ src/main/java/Project_1_Cinema_Management_Application/Guest_List.java
@@ -0,0 +1,34 @@
+/**
+ * PROBLEM STATEMENT:
+ * -------------------
+ * You have a guest list of the people who registered their stay at the hotel. There are 4 rooms in the hotel: 1 single room, 2 double rooms and 1 family room for three guests. The guest list consists of 4 lines. Each line contains the name or the names of the guests registered together in one room.
+ *
+ * Read the names of the registered guests from the list and output each name in a separate line and in reverse order starting with the last registered guest. Note that the order of the guests registered in each room is undefined.
+ *
+ *  Report a typo
+ * Sample Input 1:
+ *
+ * Jane Kate
+ * John
+ * Mary Susan Paul
+ * Boris Ann
+ * Sample Output 1:
+ *
+ * Ann
+ * Boris
+ * Paul
+ * Susan
+ * Mary
+ * John
+ * Kate
+ * Jane
+ */
+
+
+package Project_1_Cinema_Management_Application;
+
+public class Guest_List {
+    public static void main(String[] args) {
+
+    }
+}
