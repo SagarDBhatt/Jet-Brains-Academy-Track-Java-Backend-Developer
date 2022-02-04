cc58d106ecd24a39e89a23a688dd977e5034d4c9
sagarbhatt.ons1087@gmail.com <sagarbhatt.ons1087@gmail.com>
2022-02-04 10:53:32 -0700

    Static block discussion

--- src/main/java/Project_1_Cinema_Management_Application/Practice.java
+++ src/main/java/Project_1_Cinema_Management_Application/Practice.java
@@ -1,6 +1,14 @@
 package Project_1_Cinema_Management_Application;
 
 public class Practice {
+
+    /**
+     * Concept of static block. The static block executes before the main thread execution starts.
+     */
+    static {
+        System.out.println("Inside static block");
+    }
+
     public static void main(String[] args) {
         System.out.println(9/2);
     }
